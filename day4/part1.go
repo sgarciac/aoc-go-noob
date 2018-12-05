11cc9d84e48c39642f29e7e7e1bb0b0baa0cc9ca
sergio garcia <sergio.garcia@gmail.com>
2018-12-04 21:50:09 -0500

    calculated max guard

--- day4/part1.go
+++ day4/part1.go
@@ -1,10 +1,46 @@
 package main
 
-import ("fmt");
+import (
+	"fmt"
+)
 
 func main(){
 	log := ReadLog();
+	times := make(map[int]int);
+	currentGuard := -1;
+	currentMinute := -1
+	currentState := -1
+	maxGuard := -1;
+	maxTime := -1
 	for _, logentry := range log {
-		fmt.Printf("%s\n", logentry.date);
+		switch logentry.logtype {
+		case awake:
+			newMinute := logentry.date.Minute()
+			times[currentGuard] += (newMinute - currentMinute)
+			currentMinute = newMinute
+			currentState = awake
+			if times[currentGuard] > maxTime {
+				maxGuard = currentGuard
+				maxTime = times[currentGuard]
+			}
+		case asleep:
+			currentMinute = logentry.date.Minute()
+			currentState = asleep
+		case shift:
+			if (currentGuard != -1){
+				if currentState == asleep {
+					times[currentGuard] += (60 - currentMinute)
+
+				}
+			}
+			currentGuard = logentry.guardId
+			if(logentry.date.Hour() != 0) {
+				currentMinute = 0;
+			} else {
+				currentMinute = logentry.date.Minute()
+			}
+		}
 	}
+
+	fmt.Printf("max guard: %d hours = %d\n",maxGuard, maxTime);
 }
--- day4/readinput.go
+++ day4/readinput.go
@@ -4,15 +4,28 @@ import (
 	"bufio"
 	"os"
 	"regexp"
+	"time"
+	"sort"
+	"strconv"
+)
+
+const (
+	asleep = iota
+	awake
+	shift
 )
 
 var validLogEntry = regexp.MustCompile(`^\[(\d\d\d\d-\d\d-\d\d \d\d:\d\d)\] (.*)$`)
+var validBeginShift = regexp.MustCompile(`^Guard #(\d+) begins shift$`);
+
 
 type logentry struct {
-	date string
-	content string
+	date time.Time
+	logtype int
+	guardId int
 }
 
+// returns the log, sorted
 func ReadLog() ([]logentry){
 	scanner := bufio.NewScanner(os.Stdin)
 	var log []logentry
@@ -20,12 +33,28 @@ func ReadLog() ([]logentry){
 		str := scanner.Text()
 		if(len(str) > 0){
 			match := validLogEntry.FindStringSubmatch(str)
-			date := match[1]
-			content := match[2]
+			date, _ := time.Parse("2006-01-02 15:04", match[1])
+			// parse logtype
+			guardId := 0
+			logtype := -1
+			if match[2] == "wakes up" {
+				logtype = awake
+			} else if match[2] == "falls asleep" {
+				logtype = asleep
+			} else {
+				logtype = shift
+				matchShift := validBeginShift.FindStringSubmatch(match[2]);
+				guardId, _ = strconv.Atoi(matchShift[1]);
+			}
+
 			log = append(log, logentry{
 				date,
-				content})
+				logtype,
+				guardId})
 		}
 	}
-	return log
+	sort.Slice(log,func(i, j int) bool {
+		return log[i].date.Before(log[j].date);
+	});
+	return log;
 }
